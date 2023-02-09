6f024b06ebe2ba8b60e39f6e683ba19c7eba8d73
Eduardo Escárez <eduardoe@gmail.com>
2023-02-09 19:35:33 -0300

    Sección 76 - Axios

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,3 +1,26 @@
+<script setup>
+
+import axios from "axios";
+import { ref } from "vue";
+
+const dataPokemon = ref([]);
+
+
+const getData = async () => {
+    try {
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`);
+        dataPokemon.value = data
+        console.log(dataPokemon.value);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getData();
+
+
+</script>
+
 <template>
     <h1>Nombre del pokemon: {{ $route.params.pokemon }}</h1>
 </template>
